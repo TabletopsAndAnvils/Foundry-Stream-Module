7ccc7e858f0103bdb8ac23316ce958fd39f8fc6a
TabletopsAndAnvils <76455772+TabletopsAndAnvils@users.noreply.github.com>
2021-01-18 16:40:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@
 import { fsMod } from "./scripts/fromTwitch.js";
 import {getSetting, registerSettings} from "./scripts/settings.js";
 
-/*  Need to set myChannel and filter name from configuration. getSetting(twitchChannel) will return value for 
+/*  Need to set myChannel and filter name from configuration. getSetting(twitchChannel)(getSetting is in settings.js) will return value for 
     myChannel. getSetting(twitchUN) will return value for filterName. However, they cannot be called prior
-    as below with var = myChannel = getSetting(twitchChannel) etc as they haven't been loaded yet. When 
+    as below with var myChannel = getSetting(twitchChannel) etc as they haven't been loaded yet. When 
     I attempt to load them in a hook (init, ready or otherwise) I don't seem to be doing it correctly.
 */
 var myChannel =  "tabletopsandanvils";
