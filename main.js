66e6060ce8f06b9608e48af71c99e0b0514b659d
TabletopsAndAnvils <76455772+TabletopsAndAnvils@users.noreply.github.com>
2021-01-26 23:44:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// (F O U N D R Y - S T R E A M - M O D   0 . 1 . 4)
+// (F O U N D R Y - S T R E A M - M O D   0 . 1 . 4r)
 
 import { fsMod } from "./scripts/streamTwitch.js";
 import { registerSettings } from "./scripts/settings.js";
